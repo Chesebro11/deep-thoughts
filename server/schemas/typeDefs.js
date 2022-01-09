e3ff8d49d232ef7e34485e857fc56c6b35c9ab14
unknown <chesebrojared@gmail.com>
2022-01-09 09:03:09 -0800

    token authentication

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,6 +27,11 @@ const typeDefs = gql`
     thoughts: [Thought]
     friends: [User]
   }
+
+  type Auth {
+      token: ID!
+      user: User
+  }
   
   type Query {
     users: [User]
@@ -36,8 +41,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String!, password: String!): User
-    addUser(username: String!, email: String!, password: String!): User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
