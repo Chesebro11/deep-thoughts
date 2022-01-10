009ea2965a8737b0f5fb9fa0945c6b23c9214634
unknown <chesebrojared@gmail.com>
2022-01-10 05:57:15 -0800

    somehow lost entire database :(

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,6 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-  type Thought {
-      _id: ID
-      thoughtText: String
-      createdAt: String
-      username: String
-      reactionCount: Int
-      reactions: [Reaction]
-  }
-
-  type Reaction {
-      _id: ID
-      reactionBody: String
-      createdAt: String
-      username: String
-  }
-
   type User {
     _id: ID
     username: String
@@ -28,11 +10,27 @@ const typeDefs = gql`
     friends: [User]
   }
 
-  type Auth {
-      token: ID!
-      user: User
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
   }
-  
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me: User
     users: [User]
